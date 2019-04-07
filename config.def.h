1229bff302b6191e70b6a9ee95012fb6bc21f2f6
philor1 <scheibe.philipp@gmail.com>
2019-04-07 03:21:23 +0200

    patch switchcol on vanilla-dwm

--- config.def.h
+++ config.def.h
@@ -64,6 +64,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_n,      switchcol,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
