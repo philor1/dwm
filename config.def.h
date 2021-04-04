f3cf55b7d0c60d452872ffd55ee1615c7a030e65
philor1 <scheibe.philipp@gmail.com>
2021-04-05 00:36:56 +0200

    onebar patched

--- config.def.h
+++ config.def.h
@@ -72,6 +72,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_b,      toggleebar,     {0} },
+	{ MODKEY|ControlMask,           XK_b,      toggleebar,     {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
--- dwm.c
+++ dwm.c
@@ -1856,7 +1856,7 @@ tile(Monitor *m)
 	}
 
 	if(m->gappx == 0) {
-		if(abs(m->showbar) == 0) {
+		if(abs(m->showbar) + abs(m->showebar) == 0) {
 			y1 -= topbar ? borderpx : 0;	h1 += borderpx;
 			y2 -= topbar ? borderpx : 0;	h2 += borderpx;
 		}
@@ -1920,6 +1920,7 @@ togglebar(const Arg *arg)
 	selmon->showbar = selmon->pertag->showbars[selmon->pertag->curtag] = !selmon->showbar;
 	updatebarpos(selmon);
 	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, bh);
+	XMoveResizeWindow(dpy, selmon->ebarwin, selmon->wx, selmon->eby, selmon->ww, bh);
 	arrange(selmon);
 }
 
@@ -1929,6 +1930,7 @@ toggleebar(const Arg *arg)
 	selmon->showebar = selmon->pertag->showebars[selmon->pertag->curtag] = !selmon->showebar;
     updatebarpos(selmon);
     XMoveResizeWindow(dpy, selmon->ebarwin, selmon->wx, selmon->eby, selmon->ww, bh);
+	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, bh);
     arrange(selmon);
 }
 
@@ -2106,19 +2108,25 @@ updatebarpos(Monitor *m)
 {
 	m->wy = m->my;
 	m->wh = m->mh;
-	if (m->showbar) {
+	if ((m->showbar) && (m->showebar)){
+		m->wh -= 2 * bh;
+		m->wy = m->topbar ? m->wy + 2 * bh : m->wy;
+		m->by = m->topbar ? m->wy - bh : m->wy + m->wh;
+		m->eby =  m->topbar ? m->wy - 2 * bh : m->wy + m->wh + bh;
+	} else if ((m->showbar) && !(m->showebar)) {
 		m->wh -= bh;
-		m->by = m->topbar ? m->wy : m->wy + m->wh;
-		m->wy = m->topbar ? m->wy + bh : m->wy;
-	} else
-		m->by = -bh;
-    if(m->showebar) {
+		m->wy = m->topbar ? m->wy + bh: m->wy;
+		m->by = m->topbar ? m->wy - bh : m->wy + m->wh;
+		m->eby = - bh;
+	} else if (!(m->showbar) && (m->showebar)) {
 		m->wh -= bh;
-        m->eby = topbar ? m->wy + m->wh : m->wy;
-		m->wy = m->topbar ? m->wy : m->wy + bh;
-	}
-	else
-		m->eby = -bh;
+		m->wy = m->topbar ? m->wy + bh: m->wy;
+		m->eby = m->topbar ? m->wy - bh : m->wy + m->wh;
+		m->by = - bh;
+	} else {
+		m->eby = - bh;
+		m->by = - bh;
+    }
 }
 
 void
