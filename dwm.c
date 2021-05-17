96fd0e77c223a2f9c35640ecb48124f28e830822
philor1 <scheibe.philipp@gmail.com>
2021-05-18 00:41:13 +0200

    clientindicators-patch

--- dwm.c
+++ dwm.c
@@ -1132,6 +1132,7 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
+	int indn;
 	int w, x = 0, stw = 0;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
@@ -1152,6 +1153,7 @@ drawbar(Monitor *m)
 		if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
 		continue;
 
+		indn = 0;
 		w = TEXTW(tags[i]);
 		if (m->tagset[m->seltags] & 1 << i) {
 			drw_setscheme(drw, scheme[SchemeFocus]);
@@ -1164,6 +1166,12 @@ drawbar(Monitor *m)
 		} else {
 			drw_setscheme(drw, scheme[BARBORDERS == 1 ? SchemeUnfocus : SchemeBar]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+		}
+		for (c = m->clients; c; c = c->next) {
+			if ((c->tags & (1 << i)) && (indn * 3 + 2 < bh)) {
+				drw_rect(drw, x + 1, indn * 3 + 1, selmon->sel == c ? 5 : 2, 2, 1, urg & 1 << i);
+				indn++;
+			}
 		}
 		x += w;
 	}
